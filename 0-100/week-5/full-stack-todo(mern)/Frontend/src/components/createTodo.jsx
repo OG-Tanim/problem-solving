30448d9848e6eeb1c23e538bb3fbdadf6f9c014a
og_tanim <tanimahmedshad@gmail.com>
2026-01-10 12:36:22 +0600

    week-5(2): macbook

--- 0-100/week-5/full-stack-todo(mern)/Frontend/src/components/createTodo.jsx
+++ 0-100/week-5/full-stack-todo(mern)/Frontend/src/components/createTodo.jsx
@@ -1,13 +1,44 @@
+import { use, useState } from "react";
+
 export function CreateTodo() {
+  const [input, setInput] = useState({});
+
+  function newTodo() {
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: input.title,
+        description: input.description,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then(() => {
+      alert("New todo created");
+    });
+  }
+
   return (
     <div>
-      <input type="text" placeholder="please input a title" />
+      <input
+        type="text"
+        placeholder="please input a title"
+        onChange={(e) => {
+          setInput({ title: e.target.value });
+        }}
+      />
       <br />
       <br />
-      <input type="text" placeholder="please input a description" />
+      <input
+        type="text"
+        placeholder="please input a description"
+        onChange={(e) => {
+          setInput({ description: e.target.value });
+        }}
+      />
       <br />
       <br />
-      <button>Add todo</button>
+      <button onClick={newTodo}>Add todo</button>
     </div>
   );
 }
